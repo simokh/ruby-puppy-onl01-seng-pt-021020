bc2cabd938aa7c6b0c2bff2ddac725c9257b11b4
Simo Khouili <m.simokhouili@gmail.com>
2020-03-07 04:30:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,50 +1,11 @@
-# class Dog
-#
-#   attr_accessor :name
-#   @@all= []
-#
-#
-#   def initialize(name)
-#     @name
-#     self.save
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-#
-#   def self.clear_all
-#     @@all.clear
-#   end
-#
-#   # def self.print_all
-#   #   @@all.each do |dog|
-#   #     puts Dog.name
-#   #   end
-#   # end
-#
-#
-#   def self.print_all
-#     @@all.each do |dog|
-#       puts dog.name
-#     end
-# end
-#
-#   def save
-#     @@all << self
-#   end
-#
-#   def name
-#     @name
-#   end
-#
-# end
-
 class Dog
-  @@all = []
+
+  
+  @@all= []
+
 
   def initialize(name)
-    @name = name
+    @name
     self.save
   end
 
@@ -53,8 +14,15 @@ class Dog
   end
 
   def self.clear_all
-    @@all = []
+    @@all.clear
   end
+
+  # def self.print_all
+  #   @@all.each do |dog|
+  #     puts Dog.name
+  #   end
+  # end
+
 
   def self.print_all
     @@all.each do |dog|
@@ -70,4 +38,36 @@ end
     @name
   end
 
-end 
+end
+
+# class Dog
+#   @@all = []
+# 
+#   def initialize(name)
+#     @name = name
+#     self.save
+#   end
+# 
+#   def self.all
+#     @@all
+#   end
+# 
+#   def self.clear_all
+#     @@all = []
+#   end
+# 
+#   def self.print_all
+#     @@all.each do |dog|
+#       puts dog.name
+#     end
+# end
+# 
+#   def save
+#     @@all << self
+#   end
+# 
+#   def name
+#     @name
+#   end
+# 
+# end 
